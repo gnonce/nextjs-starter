d192c5449da462d6f4085cc15017f5ce2a546e99
kajumito <kasper@gnonce.com>
2019-06-02 11:05:03 +0800

    workbox offline support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 const withTypescript = require('@zeit/next-typescript');
+const withOffline = require('next-offline');
 
-module.exports = withTypescript({
+const nextConfig = {
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      },
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+        handler: 'CacheFirst'
+      },
+      {
+        urlPattern: /api/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+            headers: {
+              'x-test': 'true'
+            }
+          }
+        }
+      }
+    ]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -9,4 +45,6 @@ module.exports = withTypescript({
 
     return config;
   }
-});
+};
+
+module.exports = withOffline(withTypescript(nextConfig));
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const cacheableResponse = require('cacheable-response');
 const express = require('express');
 const next = require('next');
+const { join } = require('path');
+const { parse } = require('url');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-
 const handle = app.getRequestHandler();
 
 const ssrCache = cacheableResponse({
@@ -18,6 +19,13 @@ const ssrCache = cacheableResponse({
 
 app.prepare().then(() => {
   const server = express();
+
+  server.get('/service-worker.js', (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+    const filePath = join(__dirname, '.next', pathname);
+    app.serveStatic(req, res, filePath);
+  });
 
   server.get('/', (req, res) => ssrCache({ req, res, pagePath: '/' }));
 
